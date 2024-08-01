227fe602351a64f7b2693b2b4ca4f2693535b366
Rajni Singh <rajnisingh9031@gmail.com>
2024-08-01 09:45:54 +0530

    second1 commit

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const port = 5000;
 
 // Middleware to handle CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://gofood-sklz.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
